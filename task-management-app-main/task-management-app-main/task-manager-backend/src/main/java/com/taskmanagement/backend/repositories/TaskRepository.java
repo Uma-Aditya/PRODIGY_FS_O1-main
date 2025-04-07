0b92095ef40123c34786fcec997bb7ab9d0ec2ec
Uma-Aditya <mohanaditya706@gmail.com>
2025-04-07 19:37:54 +0530

    Delete task-management-app-main/task-management-app-main directory

--- task-management-app-main/task-management-app-main/task-manager-backend/src/main/java/com/taskmanagement/backend/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taskmanagement.backend.repositories;
-
-import com.taskmanagement.backend.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
